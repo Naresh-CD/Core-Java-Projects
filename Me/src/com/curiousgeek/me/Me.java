420e7638fa7bb34adfbc707b57ee13977b983bcf
Naresh <nareshdubbaka001@gmail.com>
2021-09-14 21:22:49 -0500

    This is simple me project

---
+++ Me/src/com/curiousgeek/me/Me.java
@@ -0,0 +1,10 @@
+package com.curiousgeek.me;
+
+public class Me {
+    public static void sayName() {
+        System.out.println("Geek");
+    }
+    public static void sayAge() {
+        System.out.println("21");
+    }
+}
