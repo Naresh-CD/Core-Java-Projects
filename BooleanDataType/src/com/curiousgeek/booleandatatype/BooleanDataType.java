e23bc788a89b431c881f3c18b89e48a3fc5f4b5b
Naresh <nareshdubbaka001@gmail.com>
2021-09-14 21:47:52 -0500

    This is simple boolean data type project

---
+++ BooleanDataType/src/com/curiousgeek/booleandatatype/BooleanDataType.java
@@ -0,0 +1,11 @@
+package com.curiousgeek.booleandatatype;
+
+public class BooleanDataType {
+    public static void main(String[] args) {
+        boolean isAlive = true;
+        if(isAlive)
+            System.out.println("Alive");
+        else
+            System.out.println("Not Alive");
+    }
+}
