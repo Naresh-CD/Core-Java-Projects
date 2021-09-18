dd3ddcfa51260f9ab00953bbee3baba81005bce5
Naresh <nareshdubbaka001@gmail.com>
2021-09-17 21:16:01 -0500

    This is simple Constant Variable project

---
+++ ConstantVariable/src/com/curiousgeek/constantvariable/Main.java
@@ -0,0 +1,19 @@
+package com.curiousgeek.constantvariable;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    final String COMPANY_NAME;
+        COMPANY_NAME = "Curious Geek Java";
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+        System.out.println(COMPANY_NAME);
+    }
+}
