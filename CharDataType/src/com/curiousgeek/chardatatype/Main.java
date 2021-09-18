97264385de9ef76662bfa56a0f6688de6209ce19
Naresh <nareshdubbaka001@gmail.com>
2021-09-17 21:14:21 -0500

    This is simple Char Data Type project

---
+++ CharDataType/src/com/curiousgeek/chardatatype/Main.java
@@ -0,0 +1,22 @@
+package com.curiousgeek.chardatatype;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char c1 = 'A';
+        char c2 = 65;
+        char c3 = '\u0041';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        char c4 = 'B';
+        char c5 = 66;
+        char c6 = '\u2354';
+
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+    }
+}
