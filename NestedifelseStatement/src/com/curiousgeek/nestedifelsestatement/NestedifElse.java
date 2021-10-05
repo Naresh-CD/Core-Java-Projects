5c30f711270cd2cf6c670206a8fe8b9a66d6bef6
Naresh <nareshdubbaka001@gmail.com>
2021-10-05 12:21:22 -0500

    This is simple nested if esle project

---
+++ NestedifelseStatement/src/com/curiousgeek/nestedifelsestatement/NestedifElse.java
@@ -0,0 +1,18 @@
+package com.curiousgeek.nestedifelsestatement;
+
+import java.util.Scanner;
+
+public class NestedifElse {
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        if (n > 10) {
+            System.out.println(n + " " + "is greater than 10");
+        } else if (n == 10) {
+            System.out.println(n + " " + "is equal to 10");
+        } else {
+            System.out.println(n + " " + "is less than 10");
+        }
+    }
+}
